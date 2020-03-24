d808b752858412050a8474b43bd3f006a9e6b1d1
Ben Larson <benlarson_fullstack@yahoo.com>
2020-03-24 10:45:08 -0500

    hehehe

---
+++ ClassRoster/src/test/java/com/bl/ClassRoster/ClassRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.ClassRoster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
